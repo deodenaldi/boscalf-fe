3afbd2886e0b332ea947e974404673e034a8eb9e
deodenaldi <deodenaldi@gmail.com>
2023-02-14 09:46:10 +0700

    fe-commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderBoscalf />
+    <HeroBoscalf />
+    <BannerBoscalf />
+    <InstagramBoscalf />
+    <PartnerBoscalf />
+    <FooterBoscalf />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderBoscalf from '@/components/HeaderBoscalf.vue'
+import HeroBoscalf from '@/components/HeroBoscalf.vue'
+import BannerBoscalf from '@/components/BannerBoscalf.vue'
+import InstagramBoscalf from '@/components/InstagramBoscalf.vue'
+import PartnerBoscalf from '@/components/PartnerBoscalf.vue'
+import FooterBoscalf from '@/components/FooterBoscalf.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderBoscalf,
+    HeroBoscalf,
+    BannerBoscalf,
+    InstagramBoscalf,
+    PartnerBoscalf,
+    FooterBoscalf,
+  },
+}
+</script>
